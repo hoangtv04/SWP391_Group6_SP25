f19b71a9b41740fc1e9573eebf7a3fd91c97b502
LongBMH182468 <longbmhe182468@fpt.edu.vn>
2025-03-03 00:06:22 +0700

    Update Movie.java

--- src/java/model/Movie.java
+++ src/java/model/Movie.java
@@ -1,30 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package model;
 
 import java.util.Date;
 
+/**
+ *
+ * @author tovie
+ */
 public class Movie {
-    private int movieID;
+    private int movieId;
     private String title;
     private String genre;
     private int duration;
     private Date releaseDate;
+    private String description;
+    
 
-    public Movie() {}
+   
+    public Movie() {
+    }
 
-    public Movie(int movieID, String title, String genre, int duration, Date releaseDate) {
-        this.movieID = movieID;
+    
+    public Movie(int movieId, String title, String genre, int duration, Date releaseDate, String description) {
+        this.movieId = movieId;
         this.title = title;
         this.genre = genre;
         this.duration = duration;
         this.releaseDate = releaseDate;
+        this.description = description;
+        
     }
 
+    
     public int getMovieID() {
-        return movieID;
+        return movieId;
     }
 
-    public void setMovieID(int movieID) {
-        this.movieID = movieID;
+    public void setMovieID(int movieId) {
+        this.movieId = movieId;
     }
 
     public String getTitle() {
@@ -58,4 +74,25 @@ public class Movie {
     public void setReleaseDate(Date releaseDate) {
         this.releaseDate = releaseDate;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" + "movieId=" + movieId + ", title=" + title + ", genre=" + genre + ", duration=" + duration + ", releaseDate=" + releaseDate + ", description=" + description + '}';
+    }
+
+   
+
+   
+
+    
+
+    
 }
